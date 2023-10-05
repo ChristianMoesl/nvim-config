aba72e2dbc7486d2933a5430d406640665980956
Christian Mösl <christian.moesl@live.at>
2023-10-05 08:36:11 +0200

    feat: add octo and remove lazygit keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,12 +8,11 @@ vim.keymap.del("n", "<C-Up>")
 vim.keymap.del("n", "<C-Down>")
 vim.keymap.del("n", "<C-Left>")
 vim.keymap.del("n", "<C-Right>")
--- vim.keymap.set("n", "<C-Shift-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
--- vim.keymap.set("n", "<C-Shift-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
--- vim.keymap.set("n", "<C-Shift-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
--- vim.keymap.set("n", "<C-Shift-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
-
 vim.keymap.set("n", "<A-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
 vim.keymap.set("n", "<A-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
 vim.keymap.set("n", "<A-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
 vim.keymap.set("n", "<A-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+
+-- Remove lazygit keymaps
+vim.keymap.del("n", "<leader>gg")
+vim.keymap.del("n", "<leader>gG")
---
+++ lua/plugins/octo.lua
@@ -0,0 +1,9 @@
+return {
+  "pwntester/octo.nvim",
+  event = "VeryLazy",
+  requires = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
+}
