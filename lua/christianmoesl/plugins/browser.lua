6820818671580ceaa0802c45f8814a75b043ad59
Christian Mösl <christian.moesl@e-mundo.de>
2023-11-08 22:38:00 +0100

    feat: use oil.nvim instead of neo-tree as browser

--- lua/christianmoesl/plugins/browser.lua
+++ lua/christianmoesl/plugins/browser.lua
@@ -1,32 +1,78 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  cmd = "Neotree",
-  cond = require("christianmoesl.util").is_full_profile,
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-  },
-  events = "VeryLazy",
-  keys = {
-    {
-      "<leader>fe",
-      function()
-        require("neo-tree.command").execute({
-          toggle = true,
-          dir = vim.loop.cwd(),
-        })
-      end,
-      desc = "Explorer NeoTree",
+  {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = false,
+    keys = {
+      {
+        "<leader>fb",
+        "<cmd>Oil<cr>",
+        desc = "File Browser",
+      },
+    },
+    opts = {
+      -- Keymaps in oil buffer. Can be any value that `vim.keymap.set` accepts OR a table of keymap
+      -- options with a `callback` (e.g. { callback = function() ... end, desc = "", mode = "n" })
+      -- Additionally, if it is a string that matches "actions.<name>",
+      -- it will use the mapping at require("oil.actions").<name>
+      -- Set to `false` to remove a keymap
+      -- See :help oil-actions for a list of all available actions
+      keymaps = {
+        ["g?"] = "actions.show_help",
+        ["<CR>"] = "actions.select",
+        -- ["<C-s>"] = "actions.select_vsplit",
+        -- ["<C-h>"] = "actions.select_split",
+        -- ["<C-t>"] = "actions.select_tab",
+        -- ["<C-p>"] = "actions.preview",
+        ["<C-s>"] = false,
+        ["<C-h>"] = false,
+        ["<C-t>"] = false,
+        ["<C-p>"] = false,
+        ["<C-c>"] = "actions.close",
+        -- ["<C-l>"] = "actions.refresh",
+        ["<C-l>"] = false,
+        ["<leader>or"] = "actions.refresh",
+        ["-"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+        ["`"] = "actions.cd",
+        ["~"] = "actions.tcd",
+        ["gs"] = "actions.change_sort",
+        ["gx"] = "actions.open_external",
+        ["g."] = "actions.toggle_hidden",
+        ["g\\"] = "actions.toggle_trash",
+      },
     },
   },
-  opts = {
-    filesystem = {
-      group_empty_dirs = true,
-      scan_mode = "deep",
-      filtered_items = {
-        visible = true, -- when true, they will just be displayed differently than normal items
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    cmd = "Neotree",
+    cond = false, -- require("christianmoesl.util").is_full_profile,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    },
+    events = "VeryLazy",
+    keys = {
+      {
+        "<leader>fe",
+        function()
+          require("neo-tree.command").execute({
+            toggle = true,
+            dir = vim.loop.cwd(),
+          })
+        end,
+        desc = "Explorer NeoTree",
+      },
+    },
+    opts = {
+      filesystem = {
+        group_empty_dirs = true,
+        scan_mode = "deep",
+        filtered_items = {
+          visible = true, -- when true, they will just be displayed differently than normal items
+        },
       },
     },
   },
