8c8f6b6481e2128a1cadbec4804b591fe071ffcd
Christian Mösl <christian.moesl@e-mundo.de>
2023-11-08 22:36:44 +0100

    feat: add terraform language support

---
+++ lua/christianmoesl/plugins/languages/terraform.lua
@@ -0,0 +1,44 @@
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "hcl", "terraform" },
+  desc = "terraform/hcl commentstring configuration",
+  command = "setlocal commentstring=#\\ %s",
+})
+
+return {
+  -- Ensure Terraform tools are installed
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "terraform-ls" })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "terraform",
+        "hcl",
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        terraformls = {},
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        terraform = { "terraform_fmt" },
+        tf = { "terraform_fmt" },
+        ["terraform-vars"] = { "terraform_fmt" },
+      },
+    },
+  },
+}
