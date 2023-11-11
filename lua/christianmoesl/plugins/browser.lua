fc39559747888e5da42cc8d0a52da6bad5dad3cb
Christian Mösl <christian.moesl@e-mundo.de>
2023-11-11 10:54:34 +0100

    refactor: remove unused neo-tree plugin

--- lua/christianmoesl/plugins/browser.lua
+++ lua/christianmoesl/plugins/browser.lua
@@ -43,37 +43,4 @@ return {
       },
     },
   },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    cmd = "Neotree",
-    cond = false, -- require("christianmoesl.util").is_full_profile,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    },
-    events = "VeryLazy",
-    keys = {
-      {
-        "<leader>fe",
-        function()
-          require("neo-tree.command").execute({
-            toggle = true,
-            dir = vim.loop.cwd(),
-          })
-        end,
-        desc = "Explorer NeoTree",
-      },
-    },
-    opts = {
-      filesystem = {
-        group_empty_dirs = true,
-        scan_mode = "deep",
-        filtered_items = {
-          visible = true, -- when true, they will just be displayed differently than normal items
-        },
-      },
-    },
-  },
 }
