59d7f07e085fa9521a695023dfbd81762a402d3f
Christian Mösl <christian.moesl@live.at>
2024-03-08 17:11:12 +0100

    refactor: remove blankline plugin

--- lua/christianmoesl/plugins/editor.lua
+++ lua/christianmoesl/plugins/editor.lua
@@ -5,20 +5,6 @@ return {
     event = "VeryLazy",
     opts = {},
   },
-  -- scope markers based on indentation
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    event = "VeryLazy",
-    cond = false, -- require("christianmoesl.util").is_full_profile,
-    main = "ibl",
-    opts = {
-      exclude = {
-        filetypes = {
-          "dashboard",
-        },
-      },
-    },
-  },
   -- plugin to detect indentation width automatically
   {
     "nmac427/guess-indent.nvim",
