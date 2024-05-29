5bfa8cab981e6d5077ea3d208a765f0d6523745b
Christian Mösl <christian.moesl@live.at>
2024-05-29 14:32:42 +0200

    feat: use Gradle wrapper to format with Spotless

--- spotless.sh
+++ spotless.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env zsh
 
-exec 3>&1                    # Save the place that stdout (1) points to.
-output=$(cat - | gradle spotlessApply \
-  --quiet \
-  -PspotlessIdeHook="$1" \
-  -PspotlessIdeHookUseStdIn \
-  -PspotlessIdeHookUseStdOut 2>&1 1>&3)  # Run command.  stderr is captured.
-exec 3>&-                    # Close FD #3.
+exec 3>&1 # Save the place that stdout (1) points to.
+output=$(cat - | ./gradlew spotlessApply \
+	--quiet \
+	-PspotlessIdeHook="$1" \
+	-PspotlessIdeHookUseStdIn \
+	-PspotlessIdeHookUseStdOut 2>&1 1>&3) # Run command.  stderr is captured.
+exec 3>&-                              # Close FD #3.
 
 if [[ "$output" != "IS DIRTY" ]]; then
-  echo $contents
+	echo $contents
 fi
