f9ce87de5852a86c8d10e8c915454f4d446f2e81
Christian Mösl <christian.moesl@live.at>
2024-07-17 09:51:21 +0200

    feat: adapt test keymappings

--- lua/christianmoesl/plugins/test.lua
+++ lua/christianmoesl/plugins/test.lua
@@ -33,9 +33,9 @@ return {
     config = function(_, opts) require("neotest").setup(opts) end,
     -- stylua: ignore
     keys = {
-      { "<leader>tt", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run File" },
-      { "<leader>tT", function() require("neotest").run.run(vim.loop.cwd()) end, desc = "Run All Test Files" },
-      { "<leader>tr", desc = "Run Nearest", function() require("neotest").run.run() end },
+      { "<leader>tf", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run File" },
+      { "<leader>tF", function() require("neotest").run.run(vim.loop.cwd()) end, desc = "Run All Test Files" },
+      { "<leader>tn", desc = "Run Nearest", function() require("neotest").run.run() end },
       { "<leader>ts", function() require("neotest").summary.toggle() end, desc = "Toggle Summary" },
       { "<leader>to", function() require("neotest").output.open({ enter = true, auto_close = true }) end, desc = "Show Output" },
       { "<leader>tO", function() require("neotest").output_panel.toggle() end, desc = "Toggle Output Panel" },
