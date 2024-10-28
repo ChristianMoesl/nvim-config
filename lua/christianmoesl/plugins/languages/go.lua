083ee4a62d1d546433ab0e6eb3b90963f5d70042
Christian Mösl <christian.moesl@gofore.com>
2024-10-28 09:09:59 +0100

    feat(go): integrate linter and testing

--- lua/christianmoesl/plugins/languages/go.lua
+++ lua/christianmoesl/plugins/languages/go.lua
@@ -4,7 +4,10 @@ return {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "gopls", "goimports", "gofumpt", "delve" })
+      vim.list_extend(
+        opts.ensure_installed,
+        { "gopls", "goimports", "gofumpt", "delve", "golangci-lint" }
+      )
     end,
   },
   {
@@ -78,8 +81,33 @@ return {
     },
   },
   {
+    "nvim-neotest/neotest",
+    -- ft = { "go" },
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      { "fredrikaverpil/neotest-golang", version = "*" },
+    },
+    opts = function(_, opts)
+      opts.adapters = opts.adapters or {}
+      table.insert(opts.adapters, require("neotest-golang"))
+    end,
+  },
+  {
     "leoluz/nvim-dap-go",
     ft = { "go" },
     config = function() require("dap-go").setup() end,
+  },
+  -- Install GO linter
+  {
+    "mfussenegger/nvim-lint",
+    opts = function()
+      local lint = require("lint")
+      lint.linters_by_ft = vim.tbl_deep_extend("force", lint.linters_by_ft or {}, {
+        go = { "golangcilint" },
+      })
+    end,
   },
 }
