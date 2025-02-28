a28bafac4de04985de9a00d9e3681e60048d9e18
Christian Mösl <christian.moesl@gofore.com>
2025-02-28 19:21:18 +0100

    feat: testing new javascript LSP config

--- lua/christianmoesl/plugins/languages/javascript.lua
+++ lua/christianmoesl/plugins/languages/javascript.lua
@@ -7,6 +7,7 @@ return {
         "tsx",
         "typescript",
         "vue",
+        "prisma",
       })
     end,
   },
@@ -15,10 +16,13 @@ return {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = vim.list_extend(opts.ensure_installed or {}, {
-        "vue-language-server",
-        "prettier",
         "typescript-language-server",
         "eslint_d",
+        "prettier",
+        "nxls",
+        "vue-language-server",
+        "prisma-language-server",
+        "vtsls", -- typescript functionality from VsCode
       })
     end,
   },
@@ -38,6 +42,8 @@ return {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
       local servers = {
+        prismals = {},
+        nxls = {},
         volar = {
           filetypes = {
             "typescript",
@@ -59,6 +65,45 @@ return {
             "vue",
           },
         },
+        -- vtsls = { -- not tested
+        --   -- explicitly add default filetypes, so that we can extend
+        --   -- them in related extras
+        --   filetypes = {
+        --     "javascript",
+        --     "javascriptreact",
+        --     "javascript.jsx",
+        --     "typescript",
+        --     "typescriptreact",
+        --     "typescript.tsx",
+        --   },
+        --   settings = {
+        --     complete_function_calls = true,
+        --     vtsls = {
+        --       enableMoveToFileCodeAction = true,
+        --       autoUseWorkspaceTsdk = true,
+        --       experimental = {
+        --         maxInlayHintLength = 30,
+        --         completion = {
+        --           enableServerSideFuzzyMatch = true,
+        --         },
+        --       },
+        --     },
+        --     typescript = {
+        --       updateImportsOnFileMove = { enabled = "always" },
+        --       suggest = {
+        --         completeFunctionCalls = true,
+        --       },
+        --       inlayHints = {
+        --         enumMemberValues = { enabled = true },
+        --         functionLikeReturnTypes = { enabled = true },
+        --         parameterNames = { enabled = "literals" },
+        --         parameterTypes = { enabled = true },
+        --         propertyDeclarationTypes = { enabled = true },
+        --         variableTypes = { enabled = false },
+        --       },
+        --     },
+        --   },
+        -- },
       }
       opts.servers = vim.tbl_deep_extend("force", opts.servers or {}, servers)
 
@@ -106,12 +151,35 @@ return {
     "nvim-neotest/neotest",
     dependencies = {
       "marilari88/neotest-vitest",
+      "nvim-neotest/neotest-jest",
     },
     opts = function(_, opts)
-      if opts.adapters == nil then
-        opts.adapters = {}
-      end
+      opts.adapters = opts.adapters or {}
       table.insert(opts.adapters, require("neotest-vitest"))
+      table.insert(
+        opts.adapters,
+        require("neotest-jest")({
+          jestCommand = "npx jest",
+          -- jestConfigFile = "custom.jest.config.ts",
+
+          jestConfigFile = "/Users/Christian.Moesl/Workspace/rbcp-newseditor/libs/shared/sparkle-sqs/jest.config.js",
+
+          -- jestConfigFile = function(file)
+          --   if string.find(file, "/packages/") then
+          --     return string.match(file, "(.-/[^/]+/)src") .. "jest.config.ts"
+          --   end
+          --
+          --   return vim.fn.getcwd() .. "/jest.config.ts"
+          -- end,
+          env = { CI = true },
+          cwd = function(_) return vim.fn.getcwd() end,
+        })
+      )
     end,
+  },
+  {
+    "pipoprods/nvm.nvim",
+    cond = require("christianmoesl.util").is_full_profile,
+    config = true,
   },
 }
