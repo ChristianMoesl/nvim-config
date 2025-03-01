b55909190f0ba23c575b7a9d909558da070d6519
Christian Mösl <christian.moesl@gofore.com>
2025-03-01 09:48:29 +0100

    feat: install catpuccin theme

---
+++ lua/plugins/lazyvim.lua
@@ -0,0 +1,22 @@
+return {
+  { "folke/snacks.nvim", opts = {
+
+    explorer = { enabled = false },
+  } },
+  {
+    "akinsho/bufferline.nvim",
+    enabled = false,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "catppuccin/nvim",
+    opts = {
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+    },
+  },
+}
