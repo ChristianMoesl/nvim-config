e8ea97d407a5c8e242bee572361c9457e8067a3e
Christian Mösl <christian.moesl@gofore.com>
2025-03-02 14:15:39 +0100

    feat: disable trouble and replace with quickfix list

--- lua/plugins/lazyvim.lua
+++ lua/plugins/lazyvim.lua
@@ -1,10 +1,28 @@
+-- TODO: fix floating terminal keymappings and harpoon interference
 return {
+  -- disable Snacks file explorer (replaced by Oil)
   {
     "folke/snacks.nvim",
     opts = {
       explorer = { enabled = false },
     },
   },
+  -- disable Trouble (use quickfix list instead)
+  { "folke/trouble.nvim", enabled = false },
+  {
+    "folke/todo-comments.nvim",
+    keys = {
+      { "<leader>xt", "<cmd>TodoQuickFix<cr>", desc = "Todo" },
+      {
+        "<leader>xT",
+        "<cmd>TodoQuickFix keywords=TODO,FIX,FIXME<cr>",
+        desc = "Todo/Fix/Fixme",
+      },
+      { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+      { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+    },
+  },
+  -- disable buffer status bar at the top
   {
     "akinsho/bufferline.nvim",
     enabled = false,
