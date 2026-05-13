b4c54cb489fa60ec929eb0d20582c42ce6a4726f
Christian Mösl <christian.moesl@gofore.com>
2026-05-13 13:19:18 +0200

    feat: use pi as default sidekick agent

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -3,13 +3,24 @@ return {
   ---@class sidekick.Config
   opts = {
     cli = {
+      ---@type table<string, sidekick.cli.Config|{}>
       tools = {
-        copilot = { cmd = { "copilot", "--model", "claude-opus-4.6", "--alt-screen", "on" } },
+        copilot = { cmd = { "copilot", "--model", "claude-sonnet-4.6" } },
+        opencode = { cmd = { "opencode", "--model", "github-copilot/claude-sonnet-4.6" } },
         -- copilot --help:
         -- "claude-sonnet-4.5", "claude-haiku-4.5", "claude-opus-4.5",
         -- "claude-sonnet-4", "gpt-5.2-codex", "gpt-5.1-codex-max", "gpt-5.1-codex", "gpt-5.2", "gpt-5.1",
         -- "gpt-5", "gpt-5.1-codex-mini", "gpt-5-mini", "gpt-4.1", "gemini-3-pro-preview"
       },
+    },
+  },
+  keys = {
+    {
+      "<leader>aa",
+      function()
+        require("sidekick.cli").toggle("pi")
+      end,
+      desc = "Sidekick Toggle CLI",
     },
   },
 }
